b07d716524af485e3e05c0db23609102d934a483
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-07-01 13:08:04 +0300

    Add bootstrap

--- vue-counter/src/main.js
+++ vue-counter/src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
 
 new Vue({
