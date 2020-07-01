2219942aaa62b6e36d2b02061beeb4809de7095f
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-07-01 13:47:08 +0300

    Rebuild the app for practice

--- vue-counter/src/components/Counter.vue
+++ vue-counter/src/components/Counter.vue
@@ -1,12 +1,37 @@
 <template>
-  <div>
-  
+  <div >
+  <h1>{{ counterValue }}</h1>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>
+          <b-button @click="decrement" variant="danger">Decrement</b-button>
+          <b-button @click="increment" variant="success">Increment</b-button>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Counter',
+  data(){
+    return {
+      counterValue: 0
+    }
+  }, 
+  methods: {
+    increment(){
+      this.counterValue++
+    }, 
+    decrement(){
+      if(this.counterValue >= 1) {
+        this.counterValue--
+      } else {
+        this.counterValue
+      }
+    }
+  }
   
 }
 </script>
@@ -26,5 +51,10 @@ li {
 }
 a {
   color: #42b983;
+}
+
+button {
+  margin: 0 10px; 
+  box-shadow: 2px 2px 10px gray;   
 }
 </style>
