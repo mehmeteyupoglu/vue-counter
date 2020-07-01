8324a7a8d9decd04991fec3bb197dfb7e4c01e26
Mehmet Eyüpoğlu <mehmeteyupoglu2@gmail.com>
2020-07-01 12:45:57 +0300

    Initial commit

---
+++ vue-counter/src/components/Counter.vue
@@ -0,0 +1,38 @@
+<template>
+  <b-container class="bv-example-row">
+  <b-row>
+    <b-col>
+      <b-button variant="danger">Button</b-button>
+      <b-button variant="success">Button</b-button>
+    </b-col>
+  </b-row>
+</b-container>
+</template>
+
+<script>
+export default {
+  name: 'Counter'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+button {
+  margin: 0 10px; 
+}
+</style>
